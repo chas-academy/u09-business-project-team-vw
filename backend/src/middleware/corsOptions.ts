eb0231fc3d7b906f85c5634c508c185aeba107ce
Wille1989 <W.Lundqvist@live.se>
2025-05-24 18:04:11 +0200

    trying to deploy

--- backend/src/middleware/corsOptions.ts
+++ backend/src/middleware/corsOptions.ts
@@ -1,16 +1,6 @@
-import { CorsOptions } from "cors";
+const allowedOrigins = ['http://localhost:3000'];
 
-const allowedOrigins = [
-    'http://localhost:3000',
-];
-
-export const corsOptions: CorsOptions = {
-    origin: (origin, callback) => {
-        if(!origin || allowedOrigins.includes(origin)) {
-            callback( null, true);
-        } else {
-            callback(new Error(`Not allowed by CORS from: ${origin}`));
-        }
-    },
-    credentials: true,
+export const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
 };
