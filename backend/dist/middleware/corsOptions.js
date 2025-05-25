e8873569dd8b60bda8dd102664a33d3db0022898
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-05-25 09:15:40 +0200

    remove dist folder with .gitignore

--- backend/dist/middleware/corsOptions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsOptions = void 0;
-const allowedOrigins = ['http://localhost:3000'];
-exports.corsOptions = {
-    origin: allowedOrigins,
-    credentials: true,
-};
