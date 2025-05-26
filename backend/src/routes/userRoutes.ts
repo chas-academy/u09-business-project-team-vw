8415c8ec89f6715e8e7ff1eae9b79a8215a7845a
Wille1989 <W.Lundqvist@live.se>
2025-05-26 11:53:07 +0200

    Admin routing now done, also saving to switch to user routing branch

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,17 @@
-import { Router, Request, Response } from 'express';
-import passport from 'passport';
-import { isAuthenticated } from '../middleware/isAuthenticated';
+import { Router, Request, Response, response } from 'express';
+import { isAdmin, isAuthenticated } from '../middleware/isAuthenticated';
 
 const userRouter: Router = Router();
+
+userRouter.get('/user/index', isAuthenticated, isAdmin, (req: Request, res: Response) => {
+    res.sendStatus(200);
+});
+
+userRouter.get('/user/recipes', isAuthenticated, isAdmin, (req: Request, res: Response) => {
+    res.sendStatus(200);
+});
+
+
 
 export default userRouter;
 
