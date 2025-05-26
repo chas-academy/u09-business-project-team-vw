804f4771c63500e9e3a632f5f077abc60bea7215
Wille1989 <W.Lundqvist@live.se>
2025-05-26 15:04:05 +0200

    session token now works as intended. Failed and success is tested and works

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -19,6 +19,10 @@ authRouter.get('/me', (req: Request, res: Response) => {
         res.json(req.user || { message: 'No user logged in' });
 });
 
+authRouter.get('/test-auth', isAuthenticated, (req: Request, res: Response) => {
+    res.json ({ message: 'Middleware works, you are in through the middleware!', user: req.user });
+});
+
 authRouter.get('/logout', (req: Request, res: Response) => {
     req.logout((err) => {
         if (err) return res.status(500).json({ message: 'logout failed', error: err });
