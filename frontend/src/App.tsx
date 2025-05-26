9ce7c99ff55d2e6ce05240353a69d42f7144c9d1
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-05-26 17:18:08 +0200

    add html structure for RecipeCard, add card in app.tsx route, created files for home and card styling, modified cors to allow vite frontend localhost

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss'
 import { Layout } from './components/layout/layout';
+import RecipeCard from './components/recipe-card/recipe-card';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-        {/*
-          <Route index element={<Home />} />
+          <Route index element={<RecipeCard />} />
           {/* Add more page components as needed
         */}
         </Route>
---
+++ frontend/src/components/recipe-card/recipe-card.tsx
@@ -0,0 +1,45 @@
+import { Icon } from "@iconify/react";
+
+function RecipeCard() {
+    return (
+        <div className="recipe-background">
+            <div className="upper-card">
+                <img src="https://i.imgur.com/CzXTtJV.jpg" alt="recipe" />
+            </div>
+            <div className="lower-card">
+                <h3>Rice pudding</h3>
+                <div className="types-container">
+                    <div className="recipe-type-container">
+                        <Icon className="recipe-icon" icon="mdi:leaf"></Icon>
+                        <p className="recipe-type">
+                            Vegetarian: Yes
+                        </p>
+                    </div>
+                    <div className="recipe-type-container">
+                        <Icon className="recipe-icon" icon="mdi:gluten"></Icon>
+                        <p className="recipe-type">
+                            Glutenfree: Yes
+                        </p>
+                    </div>
+                    <div className="recipe-type-container">
+                        <Icon className="recipe-icon" icon="mdi:cow"></Icon>
+                        <p className="recipe-type">
+                            Dairyfree: Yes
+                        </p>
+                    </div>
+                </div>
+                <div className="button-container">
+                    <div className="recipe-time">
+                        <Icon className="recipe-time-icon" icon="mdi:access-time"></Icon> 
+                        <p className="recipe-time-text">45 min</p>
+                    </div>
+                    <button className="card-button">
+                        <Icon className="add-recipe-icon" icon="mdi:invoice-add"></Icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RecipeCard
