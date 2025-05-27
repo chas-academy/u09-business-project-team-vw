5b5ed3746de27b14a782e9026a5f28385029b0a4
Wille1989 <W.Lundqvist@live.se>
2025-05-27 11:43:52 +0200

    added simple start function to userdash and admindash

---
+++ frontend/src/pages/admin/dashboard.tsx
@@ -0,0 +1,7 @@
+export default function adminDashboard() {
+    return (
+        <div>
+            <h1>Admin Dashboard!</h1>
+        </div>
+    );
+}
---
+++ frontend/src/pages/user/dashboard.tsx
@@ -0,0 +1,7 @@
+export default function userDashboard() {
+    return (
+        <div>
+            <h1>User dashboard</h1>
+        </div>
+    );
+}
