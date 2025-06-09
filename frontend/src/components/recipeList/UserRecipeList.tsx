7f609f66ba58e9b7bbfd44b83eb782e155599286
Wille1989 <W.Lundqvist@live.se>
2025-06-09 14:29:20 +0200

    saving FR part of Recipelist

---
+++ frontend/src/components/recipeList/UserRecipeList.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import type { RecipeList } from '../../types/RecipeList';
+import BackButton from '../button/goBack';
+
+const UserRecipeList = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [lists, setLists] = useState<RecipeList[]>([]);
+    const [message, setMessage] = useState<(string | null)>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        const fetchListAndIndex = async() => {
+
+            try {
+                setLoading(true);
+
+            const listResponse = await fetch (`${apiUrl}/recipeList/showLists`, { 
+                credentials: 'include', 
+                method: 'GET' 
+            });
+
+            if(listResponse.ok) {
+                const data = await listResponse.json();
+                setLists(data.data);
+                setMessage('List fetched!');
+            }
+
+            } catch (error) {
+                setErrorMessage('Server Error');
+                console.error(error);
+                
+            } finally {
+                setLoading(false);
+            }
+
+        }    
+
+        fetchListAndIndex();
+
+    }, [apiUrl]);
+
+    if(loading) {
+        return <p>Loading ...</p>
+    }
+
+    if (errorMessage) {
+        return <p>{errorMessage}</p>
+    }
+
+    return (
+        <div>
+            <div>
+                {message && <p>{message}</p>}
+                {lists.map((list) => (
+                <div key={list._id}>
+                    <h3>{list.title}</h3>
+                </div>
+                ))}
+            </div>
+
+            <BackButton />
+        </div>
+    )
+}
+
+export default UserRecipeList;
